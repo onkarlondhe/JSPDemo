d134871cc1a160940c0401382e9ca2d89b040e83
onkarlondhe <onkarrameshlondhe@gmail.com>
2023-09-18 17:45:55 +0530

    first commit

---
+++ src/main/java/com/jsp/LoginServlet.java
@@ -0,0 +1,50 @@
+package com.jsp;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class LoginServlet
+ */
+@WebServlet("/LoginServlet")
+public class LoginServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public LoginServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+    @Override
+    public void init() throws ServletException {
+    	// TODO Auto-generated method stub
+    	super.init();
+    	System.out.println("init method call...");
+    }
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	
+		PrintWriter out = response.getWriter();
+		String username = request.getParameter("user");
+		String password = request.getParameter("pass");
+		if(username.equalsIgnoreCase("Onkar") && password.equals("1234")) {
+			out.println("Hello "+username);
+			RequestDispatcher rd = request.getRequestDispatcher("Hello.jsp");
+			rd.forward(request, response);
+		}
+
+ 	}
+
+}
---
+++ src/main/webapp/Hello.jsp
@@ -0,0 +1,19 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+    pageEncoding="ISO-8859-1" errorPage="Error.jsp" %>
+<!DOCTYPE html>
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html" charset="ISO-8859-1">
+<title>JSP Demo</title>
+</head>
+<body>
+
+ 
+<% for(int i=1; i<=10; i++){ 
+	  out.println(5*i);  
+ }
+%>
+ 
+
+</body>
+</html>
